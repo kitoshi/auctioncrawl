ae26026669bfc7145edbf2c13c04a611b2c4b55d
kitoshi <kitoshi.charlton@gmail.com>
2021-07-09 15:26:14 -0700

    converted completely to hooks from class component

--- src/features/scraper/Scraper.js
+++ src/features/scraper/Scraper.js
@@ -1,129 +1,137 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-
-class Scraper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      apiResponse: "",
-      apiList: [],
-      itemList: [],
-      ebayResponse: "",
-      ebayList: [],
-      ebayPrice: [],
-      ebayLink: [],
-      fullTable: [],
-    };
-  }
-
-  callAPI() {
+//forgot about hooks!
+function Scraper() {
+  const [apiResponse, setapiResponse] = useState([]);
+  const [apiList, setapiList] = useState([]);
+  const [ebayResponse, setebayResponse] = useState("");
+  const [ebayPrice, setebayPrice] = useState([]);
+  const [ebayLink, setebayLink] = useState([]);
+  // const [pricediffList, setpricediffList] = useState([])
+  useEffect(() => {
+  const callAPI = () => {
     fetch("http://localhost:9000/crawlerAPI")
       .then((res) => res.text())
       .then((res) => {
-        this.setState({ apiResponse: JSON.parse(res) })
-        this.setState({
-          apiList: [...this.state.apiResponse],
-        })
-        const newArr = []
-        for (const item of this.state.apiList) {
-          newArr.push(item.title);
-          }
-        this.setState({
-          itemList: newArr,
-          })
+        setapiResponse(JSON.parse(res));
       })
       .catch((err) => err);
-  }
-
-
-
-  callEbay() {
+  };
+  
+  const callEbay = () => {
     fetch("http://localhost:9000/ebayAPI")
       .then((res) => res.text())
       .then((res) => {
-      this.setState({ ebayResponse: JSON.parse(res) })
-      this.setState({
-        ebayList: [...this.state.ebayResponse],
+        setebayResponse(JSON.parse(res));
+        
+        
+        
       })
-      const ebayArr = []
-      const ebayURL = []
-      for (let i = 0; i < this.state.ebayResponse.length; i++) {
-        if (this.state.ebayResponse[i].findItemsByKeywordsResponse[0].searchResult[0].item === undefined) {
-          ebayArr.push('N/A')
-        } else {
-        ebayArr.push(this.state.ebayResponse[i].findItemsByKeywordsResponse[0].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__)
-        }
-      }
-      for (let i = 0; i < this.state.ebayResponse.length; i++) {
-        if (this.state.ebayResponse[i].findItemsByKeywordsResponse[0].searchResult[0].item === undefined) {
-          ebayURL.push('N/A')
-        } else {
-          ebayURL.push(this.state.ebayResponse[i].findItemsByKeywordsResponse[0].searchResult[0].item[0].viewItemURL[0])
-        }
-      }
-      this.setState({
-        ebayPrice: ebayArr,
-        ebayLink: ebayURL
-      })
-      })
+
       .catch((err) => err);
-}
-
-
-
-  componentDidMount() {
-    this.callAPI();
-    this.callEbay();
-  }
-
+  };
+  
  
+  callAPI();
+  callEbay();
 
-  render() {
-    
-    /*const renderAPI = this.state.apiList.map((item) => (
-        <tr key={item.link}>
-        <th>{item.title}</th>
-        <th>{item.price}</th>
-        <th><a href={item.link}> Link</a></th>
-        </tr>
-    ))
-    const renderEbay = this.state.ebayPrice.map((item) => (
-      <tr key={item.index}>
-      <th>{item}</th>
-      </tr>
-  ))*/
-  const renderTable = this.state.apiList.map((item, idx)=> {
-    return (
-      <tr key={item.link}>
-            <th>{item.title}</th>
-        <th>{item.price}</th>
-        <th><a href={item.link}> Link</a></th>
-            <td key={idx}>{this.state.ebayPrice[idx]}</td>
-            <td key={this.state.ebayLink}><a href={this.state.ebayLink[idx]}> Link</a></td>
-        </tr>
-    )
-  })
 
-    return (
-      <div>
-        <h1>Current Active Items:</h1>
-        <ul></ul>
-        <table>
-          <tbody>
-            <tr>
-            <th>Item Name</th>
-                <th>Auction Price</th>
-                <th>Link</th>
-                <th>Ebay Price</th>
-                <th>Ebay Link</th>
-                <th>Diff</th>
-            </tr>
-            {renderTable}
-            </tbody>
-        </table>
-      </div>
-    );
+  }, []);
+useEffect(() => {
+const setEbay = () => {
+  const ebayArr = [];
+  const ebayURL = [];
+  for (let i = 0; i < ebayResponse.length; i++) {
+    if (
+      ebayResponse[i].findItemsByKeywordsResponse[0].searchResult[0]
+        .item === undefined
+    ) {
+      ebayArr.push("N/A");
+    } else {
+      ebayArr.push(
+        parseFloat(
+          ebayResponse[i].findItemsByKeywordsResponse[0].searchResult[0]
+            .item[0].sellingStatus[0].currentPrice[0].__value__
+        )
+      );
+    }
+    setebayPrice(old => [...old, ...ebayArr])
   }
+  for (let v = 0; v < ebayResponse.length; v++) {
+    if (
+      ebayResponse[v].findItemsByKeywordsResponse[0].searchResult[0]
+        .item === undefined
+    ) {
+      ebayURL.push("N/A");
+    } else {
+      ebayURL.push(
+        ebayResponse[v].findItemsByKeywordsResponse[0].searchResult[0]
+          .item[0].viewItemURL[0]
+      );
+    }
+    setebayLink(old => [...old, ...ebayURL])
+  }
+}
+setEbay()
+}, [ebayResponse])
+
+  useEffect(() => {
+    handleApiChange();
+  }, [apiResponse])
+
+  const handleApiChange = () => {
+    setapiList([...apiResponse])
+  }
+
+  /*tableMath () {
+  const priceArr = []
+  for (let j = 0; j < this.state.ebayPrice.length; j++) {
+    if (this.state.ebayPrice[j] === 'N/A') {
+      priceArr.push('N/A')
+    } else {
+      priceArr.push(this.state.ebayPrice[j])
+    }
+  }
+  this.setState({
+    pricediffList: priceArr
+  })
+}*/
+  /*this.state.apiList[i].price.match(/^[$]/gi))*/
+  
+  return (
+    
+    <div>
+      <h1>Current Active Items:</h1>
+      <ul></ul>
+      <table>
+        <tbody>
+          <tr>
+            <th>Item Name</th>
+            <th>Auction Price</th>
+            <th>Link</th>
+            <th>Ebay Price</th>
+            <th>Ebay Link</th>
+            <th>Diff</th>
+          </tr>
+          {apiList.map((item, idx) => {
+            return(
+          <tr key={item.link}>
+        <td>{item.title}</td>
+        <td>{item.price}</td>
+        <td>
+          <a href={item.link}> Link</a>
+        </td>
+        <td key={idx}>{ebayPrice[idx]}</td>
+        <td key={ebayLink}>
+          <a href={ebayLink[idx]}> Link</a>
+        </td>
+      </tr>
+            )
+    })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Scraper;
